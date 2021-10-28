0c857ac00779ff8068fa9a7ed2e7f0d2103d04c2
AnhNguyen <anhqfwork@gmail.com>
2021-10-29 04:45:48 +0700

    Update Equip Schema Condition

--- backend/models/Equipment.js
+++ backend/models/Equipment.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 
 const EquipmentSchema = new mongoose.Schema({
-    name: String,
-    img: String,
-    model: String,
-    quantity: Number,
-    category: String,
-    description: String,
-    country: String,
-    subcategory: String,
-    condition: String, 
-    brand: String
+    name: { type: String, required: true },
+    img: { type: String, required: true },
+    model: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true },
+    country: { type: String, required: true },
+    subcategory: { type: String, required: true },
+    condition: { type: String, required: true },
+    brand: { type: String, required: true },
 })
 
 module.exports = mongoose.model('Equipment', EquipmentSchema)
